124d8aeddc33b5ec14d2238d2d6916a50e0635f1
AhmadBerri <ahmadberri281197@gmail.com>
2022-08-06 18:48:04 +0300

    Login Backend API Done

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-	public function login(Request $request): \Illuminate\Http\JsonResponse
+	public function login(Request $request): JsonResponse
 	{
 		$credentials = $request->validate([
 			"email" => ["required", "email"],
