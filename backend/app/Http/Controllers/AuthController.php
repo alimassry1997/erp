989f321acc62d3a361f52773ef89d4f134e1d3f5
AhmadBerri <ahmadberri281197@gmail.com>
2022-08-10 22:06:01 +0300

    Added Relation for System Roles with Users and added an api debugger

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,30 +9,31 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-	/**
-	 * Login Process that checks for email and password and returns $user and their token
-	 * @param Request $request
-	 * @return JsonResponse
-	 */
-	public function login(Request $request): JsonResponse
-	{
-		$credentials = $request->validate([
-			"email" => ["required", "email"],
-			"password" => ["required"],
-		]);
-		if (!Auth::attempt($credentials)) {
-			return response()->json(
-				[
-					"message" => "Invalid login details",
-				],
-				401
-			);
-		}
-		$user = User::where("email", $request["email"])->firstOrFail();
-		$token = $user->createToken("auth_token")->plainTextToken;
-		return response()->json([
-			"user" => $user,
-			"access_token" => $token,
-		]);
-	}
+    /**
+     * Login Process that checks for email and password and returns $user and their token
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            "email" => ["required", "email"],
+            "password" => ["required"],
+        ]);
+        $check_if_active = array_merge($credentials, ["status" => "1"]);
+        if (!Auth::attempt($check_if_active)) {
+            return response()->json(
+                [
+                    "message" => "Invalid login details",
+                ],
+                401
+            );
+        }
+        $user = Auth::user();
+        $token = $user->createToken("auth_token")->plainTextToken;
+        return response()->json([
+            "user" => $user,
+            "access_token" => $token,
+        ]);
+    }
 }
--- backend/app/Models/SystemRole.php
+++ backend/app/Models/SystemRole.php
@@ -6,4 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SystemRole extends Model
 {
+    /**
+     * Get the users for the system role.
+     */
+    public function users(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
