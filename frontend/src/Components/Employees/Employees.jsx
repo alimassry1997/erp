8dbddff1900df459a9179bc48a06674099a10968
ahmadberri <ahmadberri_1997@hotmail.com>
2022-08-12 14:46:39 +0300

    Ali Massry branch for Hassan merge

--- frontend/src/Components/Employees/Employees.jsx
+++ frontend/src/Components/Employees/Employees.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './Employees.css'
-import './Employees.css';
-import axios from 'axios';
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Employees.css";
+import "./Employees.css";
+import axios from "axios";
+import { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Employees extends Component {
-
   state = {
     emp: [],
     loading: true,
-  }
+  };
 
   /* Image lal employee then fname then lname then email then phone then status with a button to
   whether you want to activate or deactivate an employee and a manage finally containing edit icon
@@ -20,22 +19,26 @@ class Employees extends Component {
    */
 
   async componentDidMount() {
-    const response = await axios.get('http://localhost:8080/api/employees');
+    const response = await axios.get("http://localhost:8080/api/employees");
     //  console.log(response);
     if (response.data.status === 200) {
       this.setState({
         emp: response.data.employees,
         loading: false,
       });
-     }
-
+    }
   }
 
-
   render() {
-    var emp_table= "";
+    const emp_table = "";
     if (this.state.loading) {
-        emp_table = <tr><td colSpan="6"><h2>Loading ...</h2></td></tr>
+      emp_table = (
+        <tr>
+          <td colSpan="6">
+            <h2>Loading ...</h2>
+          </td>
+        </tr>
+      );
     } else {
       emp_table = this.state.emp.map((item) => {
         return (
@@ -46,43 +49,39 @@ class Employees extends Component {
             <td>{item.last_name}</td>
             <td>{item.email}</td>
             <td>{item.phone_number}</td>
-            <td><button type='button'>View</button>
-             <Link to={`edit-employee/${item.id}`}> <button type='button'>Edit</button> </Link>
-              <button type='button'>Deactivate</button></td>
-         </tr>
+            <td>
+              <button type="button">View</button>
+              <Link to={`edit-employee/${item.id}`}>
+                {" "}
+                <button type="button">Edit</button>{" "}
+              </Link>
+              <button type="button">Deactivate</button>
+            </td>
+          </tr>
         );
       });
-      }
+    }
 
-    return <div>
-
-
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Status</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-            <th>Actions
-            </th>
-          </tr></thead>
-        <tbody>
-          {emp_table}
-        </tbody>
-      </table>
-     <Link to="/add-employee">Add</Link>
-
-    </div>;
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Status</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{emp_table}</tbody>
+        </table>
+        <Link to="/add-employee">Add</Link>
+      </div>
+    );
   }
-};
-
-function Employees() {
-  return (
-    <div className='employee'>Employees page</div>
-  )
 }
 
-export default Employees
+export default Employees;
--- frontend/src/Components/Layout/Sidebar.jsx
+++ frontend/src/Components/Layout/Sidebar.jsx
@@ -5,95 +5,61 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <>
-      <section className="side_bar">
-        <div className="top">
-          <div className="brand">
-            <img src={Logo} alt="logo" />
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-brands fa-adn"></i>
-                <Link to={"/Admins"}>
-                  <span>Admins</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-solid fa-flag"></i>
-                <Link to={"/"}>
-                  <span>Reports</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-solid fa-list-check"></i>
-                <Link to={"/Projects"}>
-                  <span>Projects</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-solid fa-people-group"></i>
-                <Link to={"/Teams"}>
-                  <span>Teams</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-solid fa-user"></i>
-                <Link to={"/Employees"}>
-                  <span>Employees</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa fa-tachometer" aria-hidden="true"></i>
-                <Link to={"/roles"}>
-                  <span>Roles</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa fa-tachometer" aria-hidden="true"></i>
-                <Link to={"/kpi"}>
-                  <span>KPI</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="links">
-            <ul>
-              <li>
-                <i className="fa-solid fa-right-from-bracket"></i>
-                <a href="#">
-                  <span>Logout</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-    </>
+    <section className="side_bar">
+      <div className="brand">
+        <img src={Logo} alt="logo" />
+      </div>
+      <ul>
+        <li>
+          <i className="fa-brands fa-adn"></i>
+          <Link to={"/admins"}>
+            <span>Admins</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa-solid fa-flag"></i>
+          <Link to={"/"}>
+            <span>Reports</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa-solid fa-list-check"></i>
+          <Link to={"/projects"}>
+            <span>Projects</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa-solid fa-people-group"></i>
+          <Link to={"/teams"}>
+            <span>Teams</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa-solid fa-user"></i>
+          <Link to={"/employees"}>
+            <span>Employees</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa fa-tachometer" aria-hidden="true"></i>
+          <Link to={"/roles"}>
+            <span>Roles</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa fa-tachometer" aria-hidden="true"></i>
+          <Link to={"/kpi"}>
+            <span>KPI</span>
+          </Link>
+        </li>
+        <li>
+          <i className="fa-solid fa-right-from-bracket"></i>
+          <a href="#">
+            <span>Logout</span>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 }
 
