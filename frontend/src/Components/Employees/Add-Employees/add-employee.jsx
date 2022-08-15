83b341ea4ef43918baee7934f09495dd63f0f695
Ali Massry <alimassry20@gmail.com>
2022-08-16 02:57:00 +0300

    popup & fixes

--- frontend/src/Components/Employees/Add-Employees/add-employee.jsx
+++ frontend/src/Components/Employees/Add-Employees/add-employee.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import swal from "sweetalert";
-import { useEffect } from "react";
 import "./add-employee.css";
 
 
