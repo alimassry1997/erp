f438855fe4d7bb9d10fa74357a4fa0b82ae6212c
AhmadBerri <ahmadberri281197@gmail.com>
2022-08-17 19:54:13 +0300

    Added Tabs for the single Team Page

--- frontend/src/Components/Teams/SingleTeam.jsx
+++ frontend/src/Components/Teams/SingleTeam.jsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Spinner from "../Layout/Spinner";
 import "./SingleTeam.css";
 import "./SingleTeamEmployees.css";
 import { HiUserGroup } from "react-icons/hi";
-import { AiOutlineTeam } from "react-icons/ai";
+import { AiOutlineProject, AiOutlineTeam, AiOutlineUser } from "react-icons/ai";
 import { FaEdit, FaPlusSquare, FaTrashAlt } from "react-icons/fa";
 import SingleTeamEmployees from "./SingleTeamEmployees";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const SingleTeam = ({
   team,
@@ -50,51 +52,66 @@ const SingleTeam = ({
             </button>
           </div>
         </header>
-        {size > 0 ? (
-          <div className="dashboard team-employees-dashboard">
-            <div className="header">
-              <h2>
-                <AiOutlineTeam />
-                Employees Assigned
-              </h2>
-              <button className="btn add-btn">
-                <FaPlusSquare />
-              </button>
-            </div>
-            <div className="table-responsive">
-              <table className="table table-team-employees">
-                <thead>
-                  <tr>
-                    <th>Image</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Email</th>
-                    <th>Phone Number</th>
-                    <th>Status</th>
-                    <th>Manage</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {employees.map((employee) => (
-                    <SingleTeamEmployees
-                      key={employee.id}
-                      image={employee.picture}
-                      firstName={employee.first_name}
-                      lastName={employee.last_name}
-                      email={employee.email}
-                      phoneNumber={employee.phone_number}
-                      status={employee.status}
-                    />
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        ) : (
-          <div className="dashboard team-employees-dashboard">
-            No Employees Assigned
-          </div>
-        )}
+        <Tabs>
+          <TabList>
+            <Tab>
+              <AiOutlineProject /> Projects
+            </Tab>
+            <Tab>
+              <AiOutlineUser /> Team Members
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Projects Here</h2>
+          </TabPanel>
+          <TabPanel>
+            {size > 0 ? (
+              <div className="dashboard team-employees-dashboard">
+                <div className="header">
+                  <h2>
+                    <AiOutlineTeam />
+                    Employees Assigned
+                  </h2>
+                  <button className="btn add-btn">
+                    <FaPlusSquare />
+                  </button>
+                </div>
+                <div className="table-responsive">
+                  <table className="table table-team-employees">
+                    <thead>
+                      <tr>
+                        <th>Image</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Status</th>
+                        <th>Manage</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {employees.map((employee) => (
+                        <SingleTeamEmployees
+                          key={employee.id}
+                          image={employee.picture}
+                          firstName={employee.first_name}
+                          lastName={employee.last_name}
+                          email={employee.email}
+                          phoneNumber={employee.phone_number}
+                          status={employee.status}
+                        />
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : (
+              <div className="dashboard team-employees-dashboard">
+                No Employees Assigned
+              </div>
+            )}
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
