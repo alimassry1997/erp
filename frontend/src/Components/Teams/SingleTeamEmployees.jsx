34250904180b5f59ef8d7f44e508e5b9638ff0e8
AhmadBerri <ahmadberri281197@gmail.com>
2022-08-18 20:12:06 +0300

    FewTweaks to the design

--- frontend/src/Components/Teams/SingleTeamEmployees.jsx
+++ frontend/src/Components/Teams/SingleTeamEmployees.jsx
@@ -1,6 +1,7 @@
 import "./SingleTeamEmployees.css";
 import { MdEmail } from "react-icons/md";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Employee = ({
   firstName,
@@ -11,31 +12,33 @@ const Employee = ({
   phoneNumber,
 }) => {
   return (
-    <div className="single-team-employee-card">
-      <div className="single-team-employee-card-image">
-        <img src={picture} alt="Employee image" />
+    <Link to={`/employees/${email}`}>
+      <div className="single-team-employee-card">
+        <div className="single-team-employee-card-image">
+          <img src={picture} alt="Employee image" />
+        </div>
+        <div className="single-team-employee-card-body">
+          <h3>
+            {firstName} {lastName}
+          </h3>
+          <p>
+            <span>
+              <BsFillTelephoneFill /> {phoneNumber}
+            </span>
+            <span>
+              <MdEmail /> {email}
+            </span>
+          </p>
+        </div>
+        <div className="single-team-employee-card-footer">
+          {status === 1 ? (
+            <span className="active-status">Active</span>
+          ) : (
+            <span className="inactive-status">Inactive</span>
+          )}
+        </div>
       </div>
-      <div className="single-team-employee-card-body">
-        <h3>
-          {firstName} {lastName}
-        </h3>
-        <p>
-          <span>
-            <BsFillTelephoneFill /> {phoneNumber}
-          </span>
-          <span>
-            <MdEmail /> {email}
-          </span>
-        </p>
-      </div>
-      <div className="single-team-employee-card-footer">
-        {status === 1 ? (
-          <span className="active-status">Active</span>
-        ) : (
-          <span className="inactive-status">Inactive</span>
-        )}
-      </div>
-    </div>
+    </Link>
   );
 };
 
