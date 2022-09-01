cd10e05f5e4cbbacb051d6872c88320a3b29b862
ahmadberri <ahmadberri_1997@hotmail.com>
2022-09-01 15:28:51 +0300

    React Native

--- frontend-mobile/App.js
+++ frontend-mobile/App.js
@@ -1,20 +1,53 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
+import ReportsScreen from "./screens/ReportsScreen";
+import { Colors } from "./constants/styles";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Friend</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primaryColor },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.whiteColor },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+function AuthenticatedStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primaryColor },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.whiteColor },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={ReportsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Navigation />
+    </>
+  );
+}
---
+++ frontend-mobile/util/auth.js
@@ -0,0 +1,1 @@
+function login(email, password, token) {}
