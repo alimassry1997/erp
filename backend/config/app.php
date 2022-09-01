a7df4ec6e2d84355b96cea311495e09d9fb33b81
ahmadberri <ahmadberri_1997@hotmail.com>
2022-09-01 20:49:32 +0300

    merge Error

--- backend/config/app.php
+++ backend/config/app.php
@@ -192,8 +192,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
     ],
-    
+
     /*
     |--------------------------------------------------------------------------
     | Class Aliases
